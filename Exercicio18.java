fbfaed3ed4baf47e4a2e50010c3c2b1812f6dd75
Alantrizotte06 <trizotte.alan@gmail.com>
2024-04-30 20:58:56 -0300

    Exercicio 18

---
+++ Exercicio18.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Exercicio18 {
+  public static void executar() {
+    Scanner leitor = new Scanner(System.in);
+    Abono abono = new Abono();
+
+    System.out.println("Informe o nome do funcionário: ");
+    abono.nomeFun = leitor.nextLine();
+
+    System.out.println("Informe a idade do funcionário: ");
+    abono.idade = leitor.nextInt();
+
+    leitor.nextLine();
+
+    System.out.println("Informe o sexo M ou F do funcionário: ");
+    abono.sexo = leitor.nextLine();
+
+    System.out.println("Informe o salário do funcionário: ");
+    abono.salarioFixo = leitor.nextDouble();
+
+    abono.ajusteAbono();
+  }
+}
