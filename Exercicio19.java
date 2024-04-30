5679b3491eb7f149f0637e671e5d00f950f0e0f7
Alantrizotte06 <trizotte.alan@gmail.com>
2024-04-30 20:58:19 -0300

    Exercicio 19

---
+++ Exercicio19.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Exercicio19 {
+  public static void executar() {
+    Scanner leitor = new Scanner(System.in);
+    Triangulo triangulo = new Triangulo();
+
+    System.out.println("Digite o comprimento do primeiro lado: ");
+    triangulo.a = leitor.nextInt();
+    System.out.println("Digite o comprimento do segundo lado: ");
+    triangulo.b = leitor.nextInt();
+    System.out.println("Digite o comprimento do terceiro lado: ");
+    triangulo.c = leitor.nextInt();
+
+    triangulo.retornaTriangulo();
+
+    leitor.close();
+  }
+}
