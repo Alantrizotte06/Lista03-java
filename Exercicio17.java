a828d2fc421e2c4620a0bac95b456d66767349cb
Alantrizotte06 <trizotte.alan@gmail.com>
2024-04-30 20:59:46 -0300

    Exercicio 17

---
+++ Exercicio17.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Exercicio17 {
+  public static void executar() {
+    Scanner leitor = new Scanner(System.in);
+    AumentoFolha aumento = new AumentoFolha();
+
+    System.out.println("Insira o nome do funcionario: ");
+    aumento.nome = leitor.nextLine();
+
+    System.out.println("Insira o salario atual: ");
+    aumento.salario = leitor.nextDouble();
+
+    System.out.println("Insira o valor do salario minimo: ");
+    aumento.salarioMin = leitor.nextDouble();
+
+    aumento.reajusteSal();
+
+    leitor.close();
+  }
+}
