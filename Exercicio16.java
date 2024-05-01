8980444daabd2ffb513f66db929fe98cb856b857
Alantrizotte06 <trizotte.alan@gmail.com>
2024-04-30 21:00:40 -0300

    Exercicio 16

---
+++ Exercicio16.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Exercicio16 {
+  public static void executar() {
+    Scanner leitor = new Scanner(System.in);
+    Reajuste reajuste = new Reajuste();
+
+    for (int cont = 0; cont < reajuste.funcionarios.length; cont++) {
+
+      System.out.println("Informe quantos salarios minimos o funcionario recebe:");
+      reajuste.salarioMinimos[cont] = leitor.nextInt();
+
+      System.out.println("Informe o salario do funcionario:");
+      reajuste.salarios[cont] = leitor.nextDouble();
+
+      leitor.nextLine();
+    }
+    reajuste.salarioRea();
+
+    leitor.close();
+  }
+}
